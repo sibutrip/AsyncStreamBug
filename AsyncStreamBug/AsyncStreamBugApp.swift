4b2bf17904c0cf8e1aaab78ba363786889124404
Cory Tripathy <ctripathy0@gmail.com>
2025-01-08 17:02:06 -0500

    Initial Commit

---
+++ AsyncStreamBug/AsyncStreamBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncStreamBugApp.swift
+//  AsyncStreamBug
+//
+//  Created by Cory Tripathy on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct AsyncStreamBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncStreamBug/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AsyncStreamBug
+//
+//  Created by Cory Tripathy on 1/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
